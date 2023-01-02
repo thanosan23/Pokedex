04ef8dfbe3eed066bf6ad0fab02bd8c89c08c282
thanosan23 <p.thanosan23@gmail.com>
2023-01-02 18:13:12 -0500

    Added colours

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -6,15 +6,15 @@ import video_games_image from "../public/video-games.svg"
 const Landing = () => {
     return (
         <>
-            <div className="flex flex-wrap justify-center items-center h-screen gap-3">
+            <div className="flex flex-wrap justify-center items-center h-screen">
                 <div className="flex flex-col gap-3">
                     <h1 className="text-5xl font-bold">Pokedex</h1>
                     <p className="text-lg">
                         A list of all the <span className="text-viridian-green font-semibold">pokemons</span>!
                     </p>
-                    <div className="py-2">
+                    <div>
                         <Link href="/pokemon">
-                            <button className="bg-viridian-green px-5 py-2 rounded shadow-lg hover:scale-110 hover:translate-y-1 hover:opacity-90 duration-300 delay-50">
+                            <button className="text-white bg-viridian-green px-5 py-2 rounded shadow-lg hover:scale-110 hover:translate-y-1 hover:opacity-90 duration-300 delay-50">
                                 Visit
                             </button>
                         </Link>
--- components/Pokedex.jsx
+++ components/Pokedex.jsx
@@ -1,6 +1,27 @@
 import React, {useState} from "react";
 import axios from "axios";
 
+const colors = {
+    'normal': '#f4f4f4',
+    'fire': '#fed7aa',
+    'water': '#bfdbfe',
+    'grass': '#dcfce7',
+    'electric': '#fef9c3',
+    'ice': '#cffafe',
+    'fighting': '#fee2e2',
+    'poison': '#fae8ff',
+    'ground': '#fef3c7',
+    'flying': '#f3e8ff',
+    'psychic': '#ffe4e6',
+    'bug': '#f6d6a7',
+    'rock': '#fed7aa',
+    'ghost': '#e9d5ff',
+    'dark': '#d6d3d1',
+    'dragon': '#ddd6fe',
+    'steel': '#e4e4e7',
+    'fairy': '#fbcfe8',
+};
+
 const Pokedex = () => {
 
     const [ pokemonData, setPokemonData ] = useState([]);
@@ -40,17 +61,17 @@ const Pokedex = () => {
                         <input className="border border-gray-900 rounded pr-5 pl-2" type="text" onChange={handleChange} placeholder="Enter Pokemon Name" required/>
                     </form>
                 </div>
-                <div className="flex justify-center">
+                <div className="flex justify-center item-center text-center">
                     {error ?
                         <h1>Not A Valid Pokemon</h1>
                         :
                         pokemonData.map((data) => (
-                            <div key={data.name} className="w-full sm:w-1/3 bg-stone-100 rounded-lg shadow-lg flex flex-col items-center justify-center text-center py-5">
+                            <div key={data.name} style={{ backgroundColor: colors[data.types[0].type.name]}} className="w-full sm:w-1/3 rounded-lg shadow-lg flex flex-col items-center justify-center text-center py-5">
                                 <h4 className="font-bold text-xl capitalize">{ data.name }</h4>
                                 <img src={data.sprites["front_default"]} width={128} height={128}/>
                                 <div className="text-left">
                                     <p className="capitalize"><span className="font-semibold">Type:</span> { data.types[0].type.name }</p>
-                                    <p><span className="font-semibold">Weight:</span> { data.weight * 0.1 } kg</p>
+                                    <p><span className="font-semibold">Weight:</span> { Math.round(data.weight * 0.1) } kg</p>
                                     <p className="capitalize"><span className="font-semibold">Height:</span> { Math.round(data.height * 3.93701) }&#8221;</p>
                                 </div>
                             </div>
